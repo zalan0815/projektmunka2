ba9905b90420548dee4e20ebf9799d3486ba0fe4
Tófalvi Zalán <tofalvi.zalan@students.jedlik.eu>
2022-03-18 08:40:52 +0100

    #5 komoly haladás

--- reszletek.js
+++ reszletek.js
@@ -1,8 +1,10 @@
 function reszletek(a){
     document.getElementById(a).classList.remove('d-none');
     document.getElementById(a).classList.add('d-block');
+    document.getElementsByTagName('body')[0].style.overflow = 'hidden';
 }
-function close(a){
+function becsuk(a){
     document.getElementById(a).classList.remove('d-block');
     document.getElementById(a).classList.remove('d-none');
+    document.getElementsByTagName('body')[0].style.overflow = 'visible';
 }
