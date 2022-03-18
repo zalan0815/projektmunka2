bcb57ea44b329142e3cce99fb1203997831a133a
Tófalvi Zalán <tofalvi.zalan@students.jedlik.eu>
2022-03-18 07:38:14 +0100

    #5 .

--- reszletek.js
+++ reszletek.js
@@ -2,3 +2,7 @@ function reszletek(a){
     document.getElementById(a).classList.remove('d-none');
     document.getElementById(a).classList.add('d-block');
 }
+function close(a){
+    document.getElementById(a).classList.remove('d-block');
+    document.getElementById(a).classList.remove('d-none');
+}
