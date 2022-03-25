4845dbc17e1be20d32159b3d41ae57fe91a70967
Tófalvi Zalán <tofalvi.zalan@students.jedlik.eu>
2022-03-25 08:41:13 +0100

    #6 majdnem kész

--- kosar.js
+++ kosar.js
@@ -1,17 +1,18 @@
 function kosarba(a){
-    if (a = "kosarba13") {
+    if (a == "kosarba13") {
         localStorage.clear;
         localStorage.setItem("kosar", 13);
-        console.log("kosarba")
-        console.log(localStorage.getItem("kosar"));
     }
-    if (a = "kosarba13pro") {
+    if (a == "kosarba13pro") {
+        localStorage.clear;
         localStorage.setItem("kosar", 14);
     }
-    if (a = "kosarba13promax") {
+    if (a == "kosarba13promax") {
+        localStorage.clear;
         localStorage.setItem("kosar", 15);
     }
-    if (a = "kosarba13mini") {
+    if (a == "kosarba13mini") {
+        localStorage.clear;
         localStorage.setItem("kosar", 12);
     }
 }
@@ -20,27 +21,45 @@ function teszt(){
     console.log(localStorage.getItem("kosar"));
 }
 
+function torles(a){
+    document.getElementById(13).classList.remove("d-block");
+    document.getElementById(13).classList.add("d-none");
+    localStorage.setItem("kosar", 0);
+}
+
 function kosarad() {
     if(localStorage.getItem("kosar") == 13){
         document.getElementById(13).classList.remove("d-none");
         document.getElementById(13).classList.add("d-block");
+        document.getElementById(0).classList.remove("d-block");
+        document.getElementById(0).classList.add("d-none");
     }
-    // if(localStorage.getItem("kosar") == 14){
-    //     document.getElementById(14).classList.remove("d-none");
-    //     document.getElementById(14).classList.add("d-block");
-    // }
-    // if(localStorage.getItem("kosar") == 15){
-    //     document.getElementById(15).classList.remove("d-none");
-    //     document.getElementById(15).classList.add("d-block");
-    // }
+    if(localStorage.getItem("kosar") == 14){
+        document.getElementById(14).classList.remove("d-none");
+        document.getElementById(14).classList.add("d-block");
+        document.getElementById(0).classList.remove("d-block");
+        document.getElementById(0).classList.add("d-none");
+    }
+    if(localStorage.getItem("kosar") == 15){
+        document.getElementById(15).classList.remove("d-none");
+        document.getElementById(15).classList.add("d-block");
+        document.getElementById(0).classList.remove("d-block");
+        document.getElementById(0).classList.add("d-none");
+    }
     if(localStorage.getItem("kosar") == 12){
         document.getElementById(12).classList.remove("d-none");
         document.getElementById(12).classList.add("d-block");
+        document.getElementById(0).classList.remove("d-block");
+        document.getElementById(0).classList.add("d-none");
     }
-        // else{
-        //     window.alert("A kosarad üres!")
-        // }
+    if(localStorage.getItem("kosar") == 0){
+        document.getElementById(0).classList.remove("d-none");
+        document.getElementById(0).classList.add("d-block");
+    }
 }
 
-
 window.onload = kosarad;
+
+function megrendeles(){
+    window.alert("Sikeresen megrendelve!")
+}
